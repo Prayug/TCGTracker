594b9e683c73c5562956143be419b0387739555e
Prayug <prayugsigdel@gmail.com>
2026-07-14 10:30:00 -0700

    fix: move auth rate limiting to login and register routes

--- backend/dist/middleware/admin.js
+++ backend/dist/middleware/admin.js
@@ -1,8 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.requireAdminUnlessDev = exports.requireAdmin = void 0;
+exports.requireAdmin = void 0;
 const env_1 = require("../config/env");
-const auth_1 = require("./auth");
 const requireAdmin = (req, res, next) => {
     var _a;
     if (((_a = req.user) === null || _a === void 0 ? void 0 : _a.username) === env_1.env.admin.username) {
@@ -12,12 +11,3 @@ const requireAdmin = (req, res, next) => {
     res.status(403).json({ error: 'Admin access required' });
 };
 exports.requireAdmin = requireAdmin;
-/** Authenticate + check admin in all environments (never bypass auth). */
-const requireAdminUnlessDev = (req, res, next) => {
-    if (env_1.env.isDevelopment) {
-        next();
-        return;
-    }
-    (0, auth_1.authenticate)(req, res, () => (0, exports.requireAdmin)(req, res, next));
-};
-exports.requireAdminUnlessDev = requireAdminUnlessDev;
